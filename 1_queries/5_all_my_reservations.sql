ad413420b128cf0c53e0684f1d16504ed6d1a787
tylerccaceres <tyler.caceres@gmail.com>
2019-07-24 19:17:19 -0400

    fix 5th query

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,8 +1,20 @@
-select r.*, p.* , avg(p_r.rating) as average_rating
-from property_reviews p_r 
-join reservations r on p_r.property_id = r.property_id
-join properties p on p_r.property_id = p.id
-where r.guest_id = 1 and end_date < now()::date
-group by r.id, p.id
-order by r.start_date
+-- select temp_avg.property_id as id, p.title as title, p.cost_per_night as cost_per_night, r.start_date as start_date, temp_avg.average as average_rating
+-- from reservations r
+-- join properties p on p.id = r.property_id
+-- join
+-- (select p_r.property_id as property_id,avg(p_r.rating) as average
+-- from property_reviews p_r
+-- group by p_r.property_id) temp_avg on temp_avg.property_id = p.id
+-- where r.guest_id = 1 and r.end_date < now()::date
+-- order by start_date
+-- limit 10;
+select p.*,r.*, temp_avg.average as average_rating
+from reservations r
+join properties p on p.id = r.property_id
+join
+(select p_r.property_id as property_id,avg(p_r.rating) as average
+from property_reviews p_r
+group by p_r.property_id) temp_avg on temp_avg.property_id = p.id
+where r.guest_id = 1 and r.end_date < now()::date
+order by start_date
 limit 10;
